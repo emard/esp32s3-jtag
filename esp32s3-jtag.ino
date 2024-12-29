0c6acade314e01a3184271f6a5488b6dc62a8434
Emard <vordah@gmail.com>
2024-12-29 01:20:23 +0100

    it works

--- esp32s3-jtag.ino
+++ esp32s3-jtag.ino
@@ -18,10 +18,21 @@ https://eloquentarduino.com/posts/esp32-cam-quickstart
 #define LED_ON  LOW
 #define LED_OFF HIGH
 
+#define PIN_TCK  1
+#define PIN_TMS  2
+#define PIN_TDI  3
+#define PIN_TDO  4
+#define PIN_TRST 5
+
 /* arguments are GPIO pin numbers like (1,2,3,4,5) */
 //   route_usb_jtag_to_gpio(      1,       2,       3,       4,        5);
 void route_usb_jtag_to_gpio(int tck, int tms, int tdi, int tdo, int srst)
 {
+  pinMode(PIN_TCK,OUTPUT);
+  pinMode(PIN_TMS,OUTPUT);
+  pinMode(PIN_TDI,OUTPUT);
+  pinMode(PIN_TDO,INPUT);
+  pinMode(PIN_TRST,OUTPUT);
   WRITE_PERI_REG(USB_SERIAL_JTAG_CONF0_REG,
     READ_PERI_REG(USB_SERIAL_JTAG_CONF0_REG)
   | USB_SERIAL_JTAG_USB_JTAG_BRIDGE_EN);
@@ -29,17 +40,18 @@ void route_usb_jtag_to_gpio(int tck, int tms, int tdi, int tdo, int srst)
   esp_rom_gpio_connect_out_signal(tck,   85, false, false);
   esp_rom_gpio_connect_out_signal(tms,   86, false, false);
   esp_rom_gpio_connect_out_signal(tdi,   87, false, false);
-  esp_rom_gpio_connect_in_signal (tdo,  251, false);
   esp_rom_gpio_connect_out_signal(srst, 251, false, false);
+  esp_rom_gpio_connect_in_signal (tdo,  251, false);
 }
 
 void setup()
 {
-  route_usb_jtag_to_gpio(1,2,3,4,5);
   pinMode(LED_BUILTIN, OUTPUT);
   digitalWrite(LED_BUILTIN, LED_ON);
+  route_usb_jtag_to_gpio(PIN_TCK, PIN_TMS, PIN_TDI, PIN_TDO, PIN_TRST);
 }
 
 void loop() {
   digitalWrite(LED_BUILTIN, (((micros()>>16) & 15)==0)^LED_OFF );
+  // digitalWrite(LED_BUILTIN, digitalRead(2)^LED_OFF); // test input pin
 }
