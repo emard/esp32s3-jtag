b4ba6047b4a73370d82c4b8af9d063d99beed588
Emard <vordah@gmail.com>
2024-12-29 09:50:39 +0100

    find the openocd

--- openocd/test.sh
+++ openocd/test.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
-OPENOCD=$(find ~/.arduino15 -name openocd -type f)
+#OPENOCD=$(find ~/.arduino15 -name openocd -type f)
+#OPENOCD=$(which openocd)
+#OPENOCD=/usr/bin/openocd
+OPENOCD=openocd
 $OPENOCD -f esp_usb_jtag.cfg -f ecp5_25f.ocd
