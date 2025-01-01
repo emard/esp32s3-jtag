d2a760d247a02c015fa3948a435403348786b986
Emard <vordah@gmail.com>
2025-01-01 12:24:13 +0100

    additional option for test with self-compiled openocd

--- openocd/test.sh
+++ openocd/test.sh
@@ -2,5 +2,6 @@
 #OPENOCD=$(find ~/.arduino15 -name openocd -type f)
 #OPENOCD=$(which openocd)
 #OPENOCD=/usr/bin/openocd
+#OPENOCD=/tmp/openocd/src/openocd
 OPENOCD=openocd
 $OPENOCD -f esp_usb_jtag.cfg -f ecp5_25f.ocd
